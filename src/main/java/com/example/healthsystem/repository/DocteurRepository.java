d341322ca331eadde4c40cae8baf31ebaf8d6685
MockingJay1710 <elmejbarotmane@gmail.com>
2024-12-27 18:29:52 +0100

    getalldoctors + getdoctorsbyspeciality

--- src/main/java/com/example/healthsystem/repository/DocteurRepository.java
+++ src/main/java/com/example/healthsystem/repository/DocteurRepository.java
@@ -2,6 +2,13 @@ package com.example.healthsystem.repository;
 
 import com.example.healthsystem.model.Docteur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DocteurRepository extends JpaRepository<Docteur, Integer> {
+    @Query("SELECT u FROM User u WHERE u.user_type = 'DOCTOR'")
+    List<Docteur> getAllDoctors();
+
+    List<Docteur> findBySpecialiteDocteur(String specialiteDocteur);
 }
--- src/main/java/com/example/healthsystem/repository/UserRepository.java
+++ src/main/java/com/example/healthsystem/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.healthsystem.repository;
 
+import com.example.healthsystem.model.Docteur;
 import com.example.healthsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,5 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.user_type != 'PATIENT'")
     List<User> findServiceProviders();
+
 
 }
